c12d9f65979f2e973f425664e9f5c93387b2b140
Umar Nazarov <u.nazarov@dmed.uz>
2025-06-06 03:04:32 +0500

    feat: migrations

---
+++ Migrations/20250531004128_AddCategoryEntityAndRelation.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BakeryApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryEntityAndRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
