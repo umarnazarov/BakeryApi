b9c035fa5308d8beb2423ea1abc122f0e1a03b55
Umar Nazarov <u.nazarov@dmed.uz>
2025-06-06 03:03:54 +0500

    feat: models

---
+++ Models/Category.cs
@@ -0,0 +1,9 @@
+// Student ID: 00013630
+namespace BakeryApi.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public List<Product> Products { get; set; } = new();
+}
---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+// Student ID: 00013630
+namespace BakeryApi.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public decimal Price { get; set; }
+
+    public int CategoryId { get; set; }
+    public Category Category { get; set; } = null!;
+}
